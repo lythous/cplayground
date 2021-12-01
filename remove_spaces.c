9086d84834a20cfd2db533e2f603b794095d7c4e
lythous <93811931+lythous@users.noreply.github.com>
2021-12-01 12:26:43 +0330

    Create remove_spaces.c

---
+++ remove_spaces.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void remove_spaces(char* str) {
+    char *fw = str;
+    while (*fw) {
+        while(*fw==' '){
+			fw++;
+        }
+        *str = *fw;
+		str++;
+		fw++;
+    }
+}
+int main(void) {
+    char *ex = malloc(sizeof(char)*64);
+	strcpy(ex, "   S A  LAM   \n s d ! $ \n \0");
+	remove_spaces(ex);
+	printf("{%s}\n", ex);
+    return 0;
+}
