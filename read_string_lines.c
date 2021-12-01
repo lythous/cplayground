d3e28f8013e07c0b4e3377412d90de8a69a64cfb
lythous <93811931+lythous@users.noreply.github.com>
2021-12-01 13:56:41 +0330

    Create read_string_lines.c

---
+++ read_string_lines.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    char *ex = malloc(sizeof(char)*64);
+	strcpy(ex, "   S A  LAM   \n s d ! $ \n \0");
+	char *line;
+	
+	/* define \n as delimiter */
+	const char * s = "\n";
+	/* get the first line */ 
+	line = strtok(ex, s);
+	/* walk through other tokens */
+	while( line != NULL ) {
+		remove_spaces(line);
+		printf( ">> %s\n", line);
+		line = strtok(NULL, s);
+	}
+	
+    return 0;
+}
