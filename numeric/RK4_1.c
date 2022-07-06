45f7b9fe98e508207a985d00ef812acf54b6843f
lythous <93811931+lythous@users.noreply.github.com>
2022-07-06 20:27:05 +0430

    Update RK4_1.c

--- numeric/RK4_1.c
+++ numeric/RK4_1.c
@@ -4,19 +4,20 @@
 void F(float, float*, float*);
 void RK4(int, float, float, float[]);
 void RK4_next(int, float, float, float*, float*);
-void print_out(int, float, float[]);
+void output(int, float, float[]);
 void add(int, float[], float[]);
 void mul(int len, float[], float);
 void copy(int, float[], float[]);
-
-/********************************************
-* Vars with capital letters denote matrices *
-********************************************/
 
 int main(void) {
     float Y_0[1] = {1};         // Initial Condition
     RK4(1, 0.0, 1.0, Y_0);
     return 0;
+}
+
+void F(float t, float Y[], float dY[]) {
+    /** y'+2*y = t^3*exp(-2*t), y(0)=1 **/
+    dY[0] = pow(t, 3)*exp(-2*t) - 2*Y[0];
 }
 
 void RK4(int len, float t_init, float t_fin, float Y_init[]) {
@@ -27,7 +28,7 @@ void RK4(int len, float t_init, float t_fin, float Y_init[]) {
 
     copy(len, Y, Y_init);
     while (t<=t_fin) {
-        print_out(len, t, Y);
+        output(len, t, Y);
         RK4_next(len, h, t, Y, Y_next);
         copy(len, Y, Y_next);
         t += h;
@@ -37,11 +38,6 @@ void RK4(int len, float t_init, float t_fin, float Y_init[]) {
      Calculate output at exactly final
      time, regardless of step size.
      **********************************/
-}
-
-void F(float t, float Y[], float dY[]) {
-    /** y'+2*y = t^3*exp(-2*t), y(0)=1 **/
-    dY[0] = pow(t, 3)*exp(-2*t) - 2*Y[0];
 }
 
 void RK4_next(int len, float h, float t, float Y[], float Y_next[]) {
@@ -80,6 +76,14 @@ void RK4_next(int len, float h, float t, float Y[], float Y_next[]) {
 
 }
 
+/*** Do whatever you want with outputs here ***/
+void output(int len, float t, float Y[]) {
+    printf("\n%f", t);
+    for (int i=0; i<len; i++) {
+        printf(",\t%f", Y[i]);
+    }
+}
+
 /******* A <- A+B *******/
 void add(int len, float A[], float B[]) {
     for (int i=0; i<len; i++) {
@@ -101,9 +105,3 @@ void copy(int len, float COPY[], float ORIGINAL[]) {
     }
 }
 
-void print_out(int len, float t, float Y[]) {
-    printf("\n%f", t);
-    for (int i=0; i<len; i++) {
-        printf(",\t%f", Y[i]);
-    }
-}
